7ca022ddb8b071e2c2bfec44f90269c7fc317e90
ak-the-dev <balaajay19@gmail.com>
2022-01-08 12:20:32 +0530

    Home Page 20%

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const ak = document.getElementById('ak');
-const ak = document.getElementById('ak');
-const ak = document.getElementById('ak');
+const time = document.getElementById('time-1');
+const log = document.getElementById('log');
+
+window.setInterval(() => {
+  var date = new Date();
+  var n = date.toLocaleDateString();
+  var currenttime = date.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: true,
+  });
+  time.value = n + ' ' + currenttime;
+}, 1000);
